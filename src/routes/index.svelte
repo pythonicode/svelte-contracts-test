5ee3c9ebea31d4bb6ccd6a4f6190355810a6855b
Anthony Riley <anthonyriley@stanford.edu>
2022-01-04 20:35:12 -0800

    Error info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,7 @@
 	import Counter from '$lib/Counter.svelte';
 	import { onMount } from 'svelte';
 	import { web3, defaultChainStore, makeContractStore } from 'svelte-web3';
-	
+
 	// No "exports" main defined in C:\Users\AJ\Desktop\Blockchain\Tutorials\svelte-contracts-test\node_modules\svelte-web3\package.json 
 	// imported from C:\Users\AJ\Desktop\Blockchain\Tutorials\svelte-contracts-test\node_modules\svelte-contracts\dist\index.js
 	import { createContract } from 'svelte-contracts';
@@ -15,6 +15,19 @@
 	import ERC20ABI from '$lib/ERC20ABI.json';
 
 	// Uncaught (in promise) Error: [svelte-web3] Cannot find Web3
+	/*
+		at init (stores.js:40)
+		at set1193Provider (stores.js:68)
+		at setProvider (stores.js:89)
+		at Proxy.setBrowserProvider (stores.js:113)
+		at index.svelte? [sm]:19
+		at run (index.mjs:18)
+		at Array.map (<anonymous>)
+		at index.mjs:1749
+		at flush (index.mjs:1057)
+		at init (index.mjs:1841)
+	*/
+
 	onMount(() => {
 		defaultChainStore.setBrowserProvider();
 		const SHIB = makeContractStore(ERC20ABI, '0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce');
